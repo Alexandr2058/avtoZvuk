1d04bfe25b3e9ee29917adb33b90dcdc8cc2c21c
Aleksandr <chepik2058@gmail.com>
2019-04-15 23:33:25 +0300

    15.04.19 finish

--- src/test/java/productsTest/ChoiceProducts.java
+++ src/test/java/productsTest/ChoiceProducts.java
@@ -2,14 +2,30 @@ package productsTest;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 import parentTest.ParentTest;
 
 public class ChoiceProducts extends ParentTest {
     @Test
     public void choiceProducts() {
         homePage.goToProductPage();
-        homePage.clickOnMenuCatalog();
-        homePage.clickOnSubMenuCatalog("Электроника");
-        homePage.clickOnSSMenuCatalog();
+        homePage.bringToElements();
+        homePage.bringToSubElements();
+        homePage.clickOnSubMenuCatalog();
+        homePage.chooseSystem();
+        homePage.chooseClickSystem();
+        parktronikSystem.findSystem();
+        parktronikSystem.chooseSystem();
+    }
+
+    @Test
+    public void checkFiltr () throws InterruptedException {
+        loginPage.openPage();
+        homePage.bringToElements();
+        homePage.bringToSubElements();
+        homePage.clickOnSubMenuCatalog();
+        homePage.chooseSystem();
+        homePage.chooseClickSystem();
+        parktronikSystem.enterPrace();
     }
 }
